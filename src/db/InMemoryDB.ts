e2d54a15666c775b4d87ded6dcff6527e7d445c8
Natalia_Loginova1 <Natalia_Loginova1@epam.com>
2023-10-10 23:19:20 +0200

    Module 5 Network

---
+++ src/db/InMemoryDB.ts
@@ -0,0 +1,16 @@
+import {IExtendedUser} from "../types/UserTypes";
+
+export const USERS: IExtendedUser[] = [
+    {
+        id: 'f5CbL3jE',
+        name: 'Ann',
+        email: 'ann@google.com',
+        hobbies: ['books', 'sport', 'dancing'],
+    },
+    {
+        id: 'ao8Xm0I8',
+        name: 'Ben',
+        email: 'ben@google.com',
+        hobbies: ['series', 'sport'],
+    },
+];
